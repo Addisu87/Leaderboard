7559fb18d64edfdedef31edb3a2614e848ee2fb9
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-06-14 23:00:59 +0300

    Add linters to the project

--- webpack.config.js
+++ webpack.config.js
@@ -7,39 +7,39 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
   },
   devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development'
-    })
+      title: 'Development',
+    }),
   ],
   devServer: {
-    static: './dist'
+    static: './dist',
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
         test: /\.(csv|tsv)$/i,
-        use: ['csv-loader']
+        use: ['csv-loader'],
       },
       {
         test: /\.xml$/i,
-        use: ['xml-loader']
-      }
-    ]
-  }
+        use: ['xml-loader'],
+      },
+    ],
+  },
 };
