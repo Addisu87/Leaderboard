fb78211a70c9d941b1dd85193205570538d5e7bf
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-06-16 01:00:34 +0300

    Refactor my code

--- modules/PlayerList.js
+++ modules/PlayerList.js
@@ -1,42 +1,32 @@
-import Store from './store.js';
+// Get form values
+const userInput = document.querySelector('#user');
+const scoreInput = document.querySelector('#score');
+const playerList = document.querySelector('#player-list');
 
-// UI class: Handle UI tasks
-class PlayerList {
-  static displayPlayer() {
-    const players = Store.getPlayers();
+// Example POST method implementation:
+const userScore = async (url) => {
+  const scoreData = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      user: userInput.value.trim(),
+      score: scoreInput.value.trim()
+    })
+  });
+  return scoreData.json();
+};
 
-    players.forEach((player) => PlayerList.addPlayerList(player));
-  }
+const loadScores = async (url) => {
+  const result = (await fetch(url)).json();
+  playerList.innerHTML = result
+    .sort((a, b) => b.player - a.player)
+    .map(
+      (player) =>
+        `<tr><td>${player.user}</td></tr> <tr><td>${player.score}</td></tr>`
+    )
+    .join();
+};
 
-  static addPlayerList(player) {
-    const list = document.querySelector('#player-list');
-    const row = document.createElement('tr');
-
-    row.innerHTML = `  
-    <td>${player.user}</td>
-    <td>${player.score}</td>
-    `;
-
-    list.appendChild(row);
-  }
-
-  static showAlert(message, className) {
-    const div = document.createElement('div');
-    div.className = `alert alert-${className}`;
-    div.appendChild(document.createTextNode(message));
-    const container = document.querySelector('#container');
-    const form = document.querySelector('#player-form');
-    container.insertBefore(div, form);
-
-    // Vanish in 2 seconds
-    setTimeout(() => document.querySelector('.alert').remove(), 2000);
-  }
-
-  // clear fields
-  static clearFields() {
-    document.querySelector('#user').value = '';
-    document.querySelector('#score').value = '';
-  }
-}
-
-export default PlayerList;
+export { userScore, loadScores };
--- src/index.js
+++ src/index.js
@@ -1,44 +1,14 @@
-import PlayerList from '../modules/PlayerList.js';
-import Store from '../modules/store.js';
 import './style.css';
 
-// Leaderboard class: Represent Leaderboard.
-class Player {
-  constructor(user, score) {
-    this.user = user;
-    this.score = score;
-  }
-}
+import loadScores from '../modules/PlayerList.js';
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
 
 // Event: Display player
-document.addEventListener('DOMContentLoaded', PlayerList.displayPlayer);
+window.addEventListener('load', () => loadScores(url));
 
 // Event: Add a player
 document.querySelector('#player-form').addEventListener('submit', (e) => {
   // prevent actual submit
   e.preventDefault();
-
-  // Get form values
-  const user = document.querySelector('#user').value;
-  const score = document.querySelector('#score').value;
-
-  // validate
-  if (user === '' || score === '') {
-    PlayerList.showAlert('Please fill in all fields ', 'danger');
-  } else {
-    // Instantiate player
-    const player = new Player(user, score);
-
-    // add player to PlayerList
-    PlayerList.addPlayerList(player);
-
-    // add player to store
-    Store.addPlayer(player);
-  }
-});
-
-// Event: Update the status
-document.querySelector('#refresh').addEventListener('click', () => {
-  // sorting the array from highest to lowest
-  Player.sort((a, b) => a.index - b.index);
 });
