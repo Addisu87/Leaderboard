e38f48f7dcf19a11994c6720a6107211f64835fe
Addisu Haile Tedla <addisu.haile@yahoo.com>
2022-06-16 19:30:25 +0300

    Create game id

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import './style.css';
 import { userScore, loadScores } from '../modules/PlayerList.js';
 import { addScore } from '../modules/score.js';
 
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ai5Nvjw0VJjtGbkLNuyF/scores';
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/uRONUHHMkIFJJAbMV4X2/scores';
+
 const playerForm = document.querySelector('#player-form');
 const refreshBtn = document.querySelector('#refresh');
 
